8a4bea1a97138a73a94ea22ce3caa8a9b6895501
Rodrigo Véliz <rvelizs@unsa.edu.pe>
2024-09-23 16:58:52 -0500

    clase usuario

--- app/src/main/java/com/example/login2/Usuario.kt
+++ app/src/main/java/com/example/login2/Usuario.kt
@@ -1,4 +1,18 @@
 package com.example.login2
 
-class Usuario {
+class Usuario (
+    val nombres: String,
+    val apellidos: String,
+    val correo: String,
+    val telefono: String,
+    val usuario: String,
+    val contraseña: String
+) {
+    fun mostrarInformacion() {
+        println("Nombres: $nombres")
+        println("Apellidos: $apellidos")
+        println("Correo: $correo")
+        println("Telefono: $telefono")
+        println("Usuario: $usuario")
+    }
 }
