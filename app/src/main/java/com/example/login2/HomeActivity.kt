e41a853631ebd19e16a5bfe346edef58e4c8b90b
Anthony <amoroccoire@unsa.edu.pe>
2024-09-25 12:09:34 -0500

    agregando mutabilidad al texto para recibir el username del login

--- app/src/main/java/com/example/login2/HomeActivity.kt
+++ app/src/main/java/com/example/login2/HomeActivity.kt
@@ -1,12 +1,24 @@
 package com.example.login2
 
 import android.os.Bundle
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
+import com.example.login2.databinding.ActivityHomeBinding
 
 class HomeActivity : AppCompatActivity() {
+    private val TAG = "HomeActivity"
+    private lateinit var binding: ActivityHomeBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        binding = ActivityHomeBinding.inflate(layoutInflater)
+        val view: View = binding.root
+        setContentView(view)
 
+        val txtMutable = binding.txtMutable
+
+        val username = intent.getStringExtra("user")
+
+        txtMutable.text = username ?: "Usuario desconocido"
 
     }
 }
